ac2b11e6d132ced0cc4d68bb06c4f4e9cf127acf
mnemonicspace <seawookie@gmail.com>
2022-11-30 21:58:04 -0500

    comments

--- devices.py
+++ devices.py
@@ -1,11 +1,14 @@
+# Base class
 class Device:
     def __init__(self, ip, platform, user, key):
 
+        # Run setters to initialize values
         self.ip = ip
         self.platform = platform
         self.user = user
         self.key = key
 
+        # IP of device
         @property
         def ip(self):
             return self._ip
@@ -17,6 +20,7 @@ class Device:
 
             self._ip = ip
 
+        # Maunufacturer Platform
         @property
         def platform(self):
             return self._platform
@@ -28,6 +32,7 @@ class Device:
 
             self._platform = platform
 
+        # SSH Keyfile location
         @property
         def key(self):
             return self._key
@@ -39,6 +44,7 @@ class Device:
 
             self._key = key
 
+        # User account to use for device connections
         @property
         def user(self):
             return self._user
@@ -50,6 +56,7 @@ class Device:
 
             self._user = user
 
+    # Method to build netmiko compatible device object
     def build(self):
         return {
             "device_type": self.platform,
@@ -60,5 +67,6 @@ class Device:
         }
 
 
+# Function to initialize new Device objects
 def create(ip, platform, user, key="~./ssh/test"):
     return Device(ip, platform, key, user)
--- sitrep.py
+++ sitrep.py
@@ -3,17 +3,20 @@ import netmiko
 
 
 def main():
+
+    # create devices
     vpn_dc1 = devices.create(
         "10.1.14.131", "paloalto_panos", "~/.ssh/test", "admin")
 
+    # run test function on device
     get_info(vpn_dc1)
-    return
 
 
+# Function to Get System Info
 def get_info(device):
-
+    # create netmiko object with device attrib
     working_dev = device.build()
-
+    # Just print the object for now, no calls yet
     print(working_dev)
 
 
