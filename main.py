ad6e991555c6385aab1bff75ef8097637cdf7aed
Matt <matthew.d.sexton@icloud.com>
2023-01-02 21:04:48 +0000

    testing syntax change

--- main.py
+++ main.py
@@ -16,10 +16,8 @@ def main():
     ]
 
     cisco_list = [
-        nhcisco.Cisco("DC1-6807", "172.16.50.60", "cisco_ios",
-                      "lab", "/Users/msexton/.ssh/lab", "AutoLab-sw1"),
-        nhcisco.Cisco("601-6807", "172.16.50.61", "cisco_ios",
-                      "lab", "/Users/msexton/.ssh/lab", "AutoLab-sw2")
+        nhcisco.Cisco("DC1-6807", "172.16.50.60", "cisco_ios", "lab", "/Users/msexton/.ssh/lab", "AutoLab-sw1"),
+        nhcisco.Cisco("601-6807", "172.16.50.61", "cisco_ios", "lab", "/Users/msexton/.ssh/lab", "AutoLab-sw2")
     ]
 
     c_command = "show standby brief"
