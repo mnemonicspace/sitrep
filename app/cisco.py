6b9ce03daab3ef339bf097ccc91bc066b60fc568
mnemonicspace <seawookie@gmail.com>
2023-01-12 11:26:07 -0500

    expect exact to expect

--- app/cisco.py
+++ app/cisco.py
@@ -121,7 +121,7 @@ class Cisco:
                 connection.sendline(f"enable {self.enable_level}")
                 connection.expect_exact('Password:')
                 connection.sendline(self.enable_pass)
-                connection.expect_exact(self.enable_prompt)
+                connection.expect(self.enable_prompt)
                 prompt = self.enable_prompt
             else:
                 connection.prompt()
